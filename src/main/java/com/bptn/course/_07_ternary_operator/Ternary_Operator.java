320bc7d737f9d0d10ebc1685d6b6de9d0455b511
Ketama Gemechu <gemechuketama@gmail.com>
2024-09-28 12:13:58 -0400

    I add ternary opertor

---
+++ src/main/java/com/bptn/course/_07_ternary_operator/Ternary_Operator.java
@@ -0,0 +1,19 @@
+package com.bptn.course._07_ternary_operator;
+
+public class Ternary_Operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =5;
+		int b =6;
+		int c =9;
+		int Max;
+		Max =(a>b ?(a>c?a:c):(b>c?b:c));
+		
+			
+			System.out.println(Max);
+		}
+
+	}
+
+
